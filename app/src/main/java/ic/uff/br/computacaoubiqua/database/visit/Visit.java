aa6cad52f10b67ca9d924f8716d4401d8c680235
Thiago Nazareth de Oliveira <thiagon.oliver@gmail.com>
2018-11-30 22:22:30 -0200

    adcionando o modelo de visita e o relacionamento com usuario

---
+++ app/src/main/java/ic/uff/br/computacaoubiqua/database/visit/Visit.java
@@ -0,0 +1,62 @@
+package ic.uff.br.computacaoubiqua.database.visit;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+import android.arch.persistence.room.Relation;
+import android.support.annotation.NonNull;
+
+import java.util.Date;
+import java.util.List;
+
+import ic.uff.br.computacaoubiqua.database.user.User;
+
+@Entity(foreignKeys = @ForeignKey(entity = User.class,
+        parentColumns = "uid",
+        childColumns = "user_id"))
+
+public class Visit {
+
+    @PrimaryKey(autoGenerate = true)
+    private int uid;
+
+    @ColumnInfo(name = "date")
+    @NonNull
+    private Date visitDate;
+
+    @ColumnInfo(name = "user_id")
+    private User user;
+
+    public Visit(@NonNull Date visitDate, @NonNull User user) {
+        this.visitDate = visitDate;
+        this.user = user;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public Date getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(Date visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+}

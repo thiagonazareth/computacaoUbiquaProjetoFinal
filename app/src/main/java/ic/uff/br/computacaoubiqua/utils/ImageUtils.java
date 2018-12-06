7369838c318becb2e6721637edb5617043e5c0c7
leandrocicco <leandrocicco@gmail.com>
2018-12-06 02:20:48 -0200

    ajustando foto para aparecer em peh e para nao sumir ao rodar a tela

--- app/src/main/java/ic/uff/br/computacaoubiqua/utils/ImageUtils.java
+++ app/src/main/java/ic/uff/br/computacaoubiqua/utils/ImageUtils.java
@@ -9,116 +9,27 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
-import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+
+import android.graphics.Matrix;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import ic.uff.br.computacaoubiqua.activities.DetailActivity;
 
 public class ImageUtils {
 
     public static final int IMAGE_MAX_WIDTH=1024;
     public static final int IMAGE_MAX_HEIGHT=768;
 
-    public static Uri getImageUri(Context inContext, Bitmap inImage) {
-        return Uri.parse(getImagePath(inContext,inImage));
-    }
-
-    public static String getImagePath(Context context, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), inImage, "Title", null);
-        return path;
-    }
-
-    public static Bitmap getBitmap(Context context, String path){
-        Uri uri = Uri.fromFile(new File(path));
-        Bitmap bitmap = null;
-        try {
-            bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return bitmap;
-    }
-
-    public static Bitmap getBitmap(Context context, Uri uri) {
-        Bitmap image = null;
-        try{
-            ParcelFileDescriptor parcelFileDescriptor = context.getContentResolver().openFileDescriptor(uri, "r");
-            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-            image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-            parcelFileDescriptor.close();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-        return image;
-    }
-
-
-    public static Bitmap getBitmap(int witdh, int height, String path) {
-        // Get the dimensions of the View
-        int targetW = witdh;
-        int targetH = height;
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(path, bmOptions);
-        return bitmap;
-    }
-
-    public static String getRealPathFromURI(Context context, Uri uri) {
-        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
-
-    public static Bitmap getBitmapFromPath(String file) {
-
-        BitmapFactory.Options bounds = new BitmapFactory.Options();
-        bounds.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(file, bounds);
-
-        BitmapFactory.Options opts = new BitmapFactory.Options();
-        Bitmap bm = BitmapFactory.decodeFile(file, opts);
-        ExifInterface exif = null;
-        try {
-            exif = new ExifInterface(file);
-        }
-        catch(Exception e){
-
-        }
-        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
-        int orientation = orientString != null ? Integer.parseInt(orientString) :  ExifInterface.ORIENTATION_NORMAL;
-
-        int rotationAngle = 0;
-        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
-        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
-        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
-
-        Matrix matrix = new Matrix();
-        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
-        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
-        return rotatedBitmap;
-    }
-
-    public static Bitmap decodeFile(File f) throws Exception{
+    public static Bitmap decodeFile(File f) throws IOException {
         Bitmap b = null;
 
         //Decode image size
@@ -145,4 +56,44 @@ public class ImageUtils {
 
         return b;
     }
+
+    public static int getRotate(String path) throws IOException {
+        ExifInterface exif = new ExifInterface(path);
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) :  ExifInterface.ORIENTATION_NORMAL;
+
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+
+        return rotationAngle;
+    }
+
+    public static String getImagePath(Context context, Uri uri){
+        String path = null;
+        String[] fileColumns = {MediaStore.Images.Media.DATA};
+        Cursor cursor = context.getContentResolver().query(uri, fileColumns, null, null, null);
+        cursor.moveToFirst();
+        int cIndex = cursor.getColumnIndex(fileColumns[0]);
+        path = cursor.getString(cIndex);
+        cursor.close();
+        return path;
+    }
+
+    public static void showImage(Context context, String path, ImageView imageView) {
+        if (path != null) {
+            File f = new File(path);
+            try {
+                Bitmap bitmap = ImageUtils.decodeFile(f);
+                imageView.setImageBitmap(bitmap);
+                int rotate = ImageUtils.getRotate(path);
+                Log.d("PHOTO", String.valueOf(rotate));
+                imageView.setRotation(rotate);
+            } catch (IOException e) {
+                Toast.makeText(context, "Erro ao carregar a foto!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
